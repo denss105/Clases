3d5db45bebd0c1f0da30cbb195d7fc62b27c6a4f
EquipoDell <EquipoDell@192.168.0.164>
2025-03-09 20:37:12 -0600

    Se creo diferentes cuentas las cuales en conjunto crean la simulacio de depositar y retirar dinero.

---
+++ src/main/java/com/mycompany/Banco.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+
+/**
+ *
+ * @author Denisse
+ */
+public class Banco {
+    
+    public static void main(String[] args) {
+        Usuario usuario =new Usuario("denisse", 1000.0);
+        CajeroAutomatico cajero = new CajeroAutomatico();
+
+        System.out.println("Saldo inicial: " + cajero.consultarSaldo(usuario));
+
+        cajero.depositar(usuario, 500.0);
+        System.out.println("Saldo después de depositar 500: " + cajero.consultarSaldo(usuario));
+
+        if (cajero.retirar(usuario, 300.0)) {
+            System.out.println("Retiro exitoso. Saldo actual: " + cajero.consultarSaldo(usuario));
+        } else {
+            System.out.println("Retiro fallido. Fondos insuficientes.");
+        }
+    }
+}
+
